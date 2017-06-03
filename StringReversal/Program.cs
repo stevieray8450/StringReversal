db14904db70ddb22c51d5a63807b9f756a3c1fe4
Stephen Boniface <stevieray8450@gmail.com>
2017-06-03 09:26:03 -0500

    first commit

---
+++ StringReversal/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace StringReversal
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string example = "mom";
+            char[] myArray = new char[example.Length];          
+            myArray = example.ToCharArray(0, example.Length);
+            char[] reverseArray = new char[example.Length];
+
+            myArray.CopyTo(reverseArray, 0);
+            Array.Reverse(reverseArray);
+
+            string reverseString = reverseArray.ToString();
+
+            Console.WriteLine(myArray);
+			Console.WriteLine(reverseArray);
+
+            if (example == reverseString)
+            {
+                Console.WriteLine("It's a palindrome!");
+            } else 
+            {
+                Console.WriteLine("Not a palindrome.");
+            }
+		}
+    }
+}
